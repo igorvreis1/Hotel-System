5cda84f8a65888452a5e0b11f1c048887b5b16df
Ruan Leite <ruanrafaelleite@gmail.com>
2019-09-26 20:59:41 -0300

    Reorganização de arquivos

--- Arquivos/Gerenciamento de dados/dados.h
+++ Arquivos/Gerenciamento de dados/dados.h
@@ -4,40 +4,38 @@
     /*
         CONSTANTES NECESSARIAS
     */
-   #define tel 12
-   #define str 50
-   #define ccpf 11
+   #define nums 12
     /*
      TIPOS DE VARIAVEIS NECESSÁRIAS
     */
    //hotel
    typedef struct
    {
-       char nome[str];
-       char razao[str];// PERGUNTAR AO MANOEL
-       char inscricao[10];//Ver na net
-       char CNPJ[17]; //VER na net
-       char endereco[str];
-       char telefone[tel];
-       char email[str];
-       char gerente[str];
-       char telGer[tel];
+       char *nome;//nome social
+       char *razao;//nome comercial
+       char inscricao[nums];
+       char CNPJ[nums];//VER na net
+       char *endereco;
+       char telefone[nums];
+       char *email;
+       char *gerente;
+       char telGer[nums];
        int checkin[2];
        int checkout[2];
        float lucro;
-       
-       int *qReservados;
+       char *produtos;
+    //    int *qReservados; falta
    }hotel;
 
    //Hóspedes
    typedef struct
    {
     unsigned int cod; 
-    char nome[str];
-    char endereco[str];
-    char cpf[ccpf]; //tipo char para ficar mais facil a contagem dos digitos
-    char telefone[tel]; //DD-NNNNNNNNN
-    char email[str];
+    char *nome;
+    char *endereco;
+    char cpf[nums]; //DDDDDDDDD-DD
+    char telefone[nums]; //DD-NNNNNNNNN
+    char *email;
     char sexo; //M - masculino; F - feminino
     char estCiv;//C - casado; S - solteiro; D - divorciado; V - viúvo
     int datNas[3];// DD/MM/AAAA
@@ -47,18 +45,18 @@
     typedef struct
     {
         unsigned int cod;
-        char desc[str];
-        char facil[str];
-        char cat[2];
+        char *desc;
+        char *facil;
+        char cat[2]; //ATENÇÃO
     }quartos;
    
     //Categorias
     typedef struct // Player Solo, luxo casal, luxo familia, normal simples, normal casal, normal familia
     {
         unsigned int cod;
-        char desc[str];
+        char *desc;
         int valor;
-        int tamanho; 
+        int tamanho;
     }categoria;
 
 #endif
