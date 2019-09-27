56811390fee888760138082dda64f992612d68c4
Ruan Leite <ruanrafaelleite@gmail.com>
2019-09-27 02:53:55 -0300

    Criado novas structs e a função editHotel

--- Arquivos/Gerenciamento de dados/dados.c
+++ Arquivos/Gerenciamento de dados/dados.c
@@ -1,2 +1,71 @@
 #include "dados.h"
 #include <stdio.h>
+
+//funções
+void editHotel(void)
+{
+    char op;
+    tipoHotel hotel;
+
+    while(1)
+    {
+        inicio: printf("Informe o nome do Hotel: ");
+        scanf("%s%*c", &hotel.nome);
+
+        printf("Informe a razao social do hotel: ");
+        scanf("%s%*c", &hotel.razao);
+
+        do{
+            printf("Informe a inscricao estadual do hotel (apenas numeros): ");
+            scanf("%13s%*c", &hotel.insc);
+        }while(strlen(hotel.insc) != 13);
+
+        do
+        {
+            printf("Informe o cnpj do hotel (apenas numeros): ");
+            scanf("%14s%*c", &hotel.endereco);
+        } while(strlen(hotel.cnpj) != 14);
+        
+
+        do
+        {
+            printf("Informe o telefone do hotel (ddd-nnnnnnnnn): ");
+            scanf("%12s%*c", &hotel.telefone);
+        } while(strlen(hotel.telefone) != nums);
+        
+
+        printf("Informe o email do hotel: ");
+        scanf("%s%*c", &hotel.email);
+
+        printf("Informe o nome do gerente do hotel: ");
+        scanf("%s%*c", &hotel.gerente);
+
+        do{
+            printf("Informe o telefone do gerente do hotel (ddd-nnnnnnnnn): ");
+            scanf("%12s%*c", &hotel.telGer);
+        }while(strlen(hotel.gerente) != nums);
+        
+
+
+        printf("Informe o horario de checkin do hotel (hh:mm): ");
+        scanf("%d:%d", &hotel.checkin[0], &hotel.checkin[1]);
+
+        printf("Informe o horario de checkout do hotel (hh:mm): ");
+        scanf("%d:%d", &hotel.checkout[0], &hotel.checkout[1]);
+
+        do{
+            printf("As informacoes estao corretas? (s - sim / n - nao): ");
+            op = getchar();
+
+            if('S' == toupper(op))
+            {
+                //salva no arquivo
+                return;//sai da funcao
+            }else
+            if('N' == toupper(op))
+                break;//volta para o topo
+
+            printf("Opcao invalida, tente novamente!\n");
+        }while(toupper(op) != 'N' && toupper(op) != 'S')
+    }
+}
--- Arquivos/Gerenciamento de dados/dados.h
+++ Arquivos/Gerenciamento de dados/dados.h
@@ -5,58 +5,97 @@
         CONSTANTES NECESSARIAS
     */
    #define nums 12
+   #define str 100
+   #define lstr 255
+   #define modulos 1 //quantidade de modulos que o programa tem
+   #define login 8 //quantidade de digitos do login
     /*
      TIPOS DE VARIAVEIS NECESSÁRIAS
     */
    //hotel
    typedef struct
    {
-       char *nome;//nome social
-       char *razao;//nome comercial
-       char inscricao[nums];
-       char CNPJ[nums];//VER na net
-       char *endereco;
+       char nome[str];//nome social
+       char razao[str];//nome comercial
+       char insc[13];
+       char cnpj[14];
+       char endereco[str];
        char telefone[nums];
-       char *email;
-       char *gerente;
+       char email[str];
+       char gerente[str];
        char telGer[nums];
        int checkin[2];
        int checkout[2];
        float lucro;
-       char *produtos;
+       char *produtos; //atenção
     //    int *qReservados; falta
-   }hotel;
+   }tipoHotel;
 
    //Hóspedes
    typedef struct
    {
-    unsigned int cod; 
-    char *nome;
-    char *endereco;
+    unsigned int codigo; 
+    char nome[str];
+    char endereco[str];
     char cpf[nums]; //DDDDDDDDD-DD
     char telefone[nums]; //DD-NNNNNNNNN
-    char *email;
+    char email[str];
     char sexo; //M - masculino; F - feminino
     char estCiv;//C - casado; S - solteiro; D - divorciado; V - viúvo
     int datNas[3];// DD/MM/AAAA
-   }hospedes;
+   }tipoHospedes;
 
     //Acomodações
     typedef struct
     {
-        unsigned int cod;
-        char *desc;
-        char *facil;
-        char cat[2]; //ATENÇÃO
-    }quartos;
+        unsigned int codigo; //numero do quarto
+        char desc[lstr];
+        char facilidade[lstr];
+        int categoria[2]; //1 digito == classe / 2 digito == tamanho
+    }tipoQuartos;
    
     //Categorias
     typedef struct // Player Solo, luxo casal, luxo familia, normal simples, normal casal, normal familia
     {
-        unsigned int cod;
-        char *desc;
-        int valor;
-        int tamanho;
-    }categoria;
+        unsigned int codigo[2]; //1 digito -> classe / 2 digito -> tamanho
+        char desc[str];
+        float valor;
+        int tamanho; //1 - 1 pessoa / 2 - casal / 3 familia
+        char tipo; //luxo, simples, normal
+    }tipoCategoria;
 
+    //produtos
+    typedef struct
+    {
+        unsigned int codigo;
+        char desc[str];
+        int estoque;
+        int EstMin;
+        float pCusto;
+        float pVenda;
+    }tipoProdutos;
+
+    //fornecedores
+    typedef struct
+    {
+        unsigned int codigo;
+        char nome[str];//social
+        char razao[str];//nome comercial
+        int insc[13];
+        char cnpj[14];
+        char endereco[str];
+        char telefone[nums];
+        char email[str];
+    }tipoFornecedores;
+
+    //funcionario
+    typedef struct
+    {
+        unsigned int codigo;
+        char nome[str];
+        char usuario[login];
+        char senha[login];
+        char perm[modulos]; //- -> sem acesso / + -> acesso
+    }tipoFuncionarios;
+    
 #endif
