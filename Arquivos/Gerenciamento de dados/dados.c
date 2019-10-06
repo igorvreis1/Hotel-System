48e20fcf3653ddaba790dc8722329102f5e334ad
Ruan Leite <ruanrafaelleite@gmail.com>
2019-10-06 01:00:03 -0300

    Adicionado função para salvar em arquivo de texto (gerente)

--- Arquivos/Gerenciamento de dados/dados.c
+++ Arquivos/Gerenciamento de dados/dados.c
@@ -1,76 +1,84 @@
 #include "dados.h"
-#include "strfuncoes.h"
+#include <string.h>
 #include <stdio.h>
 
 //FUNCOES
 void hotel(void)//editar hotel
 {
-    char op;
     tipoHotel hotel;
 
     while(1)
     {
         printf("Informe o nome do Hotel: ");
-        scanf("%[^\n]%*c", &hotel.nome);
+        fgets(hotel.nome, 99, stdin);
 
         printf("Informe a razao social do hotel: ");
-        scanf("%[^\n]%*c", &hotel.razao);
+        fgets(hotel.nome, 99, stdin);        
 
         printf("Informe o endereco do hotel: ");
-        scanf("%[^\n]%*c", &hotel.endereco);
+        fgets(hotel.nome, 99, stdin);
 
         do{
             printf("Informe a inscricao estadual do hotel (apenas numeros/13 digitos): ");
-            scanf("%13[0-9]", &hotel.insc);
-            setbuf(stdin, NULL);
+            scanf("%14[0-9]", &hotel.insc);
+            cleanBuff();
         }while(strlen(hotel.insc) != 13);
+        
+        printf("Informe o email do hotel: ");
+        fgets(hotel.email, 99, stdin);
 
         do
         {
             printf("Informe o cnpj do hotel (apenas numeros/14 digitos): ");
-            scanf("%14[0-9]", &hotel.cnpj);
-            setbuf(stdin, NULL);
+            scanf("%15[0-9]", &hotel.cnpj);
+            cleanBuff();
         } while(strlen(hotel.cnpj) != 14);
-        
-
-        printf("Informe o email do hotel: ");
-        scanf("%s%*c", &hotel.email);
-
 
         do
         {
             printf("Informe o telefone do hotel (ddnnnnnnnnn/11 digitos): ");
-            scanf("%11[0-9]", &hotel.telefone);
-            setbuf(stdin, NULL);
+            scanf("%12[0-9]", &hotel.telefone);
+            cleanBuff();
         } while(strlen(hotel.telefone) != 11);
 
-
-        printf("Informe o nome do gerente do hotel: ");
-        scanf("%s%*c", &hotel.gerente);
-
         do{
-            printf("Informe o telefone do gerente do hotel (ddnnnnnnnnn/11 digitos): ");
-            scanf("%11[0-9]", &hotel.telGer);
-            setbuf(stdin, NULL);            
-        }while(strlen(hotel.telGer) != 11);
+            printf("Informe as horas que comecam o checkin (Apenas as horas, ex: 12): ");
+            scanf("%d", &hotel.checkin.inicio);
+        }while(checkHorario(hotel.checkin.inicio, 'h'));
+        
+        do{
+            printf("Informe as horas que terminam o horario de checkin (Apenas as horas, ex: 16): ");
+            scanf("%d", &hotel.checkin.termino);
+        }while(checkHorario(hotel.checkin.termino, 'h') || hotel.checkin.inicio >= hotel.checkin.termino);
+        
+        do{
+            printf("Informe o horario de checkout do hotel (hh:mm): ");
+            scanf("%d:%d", &hotel.checkout[0], &hotel.checkout[1]);
+        }while(checkHorario(hotel.checkout[0], 'h') || checkHorario(hotel.checkout[1], 'm'));
 
-
-        printf("Informe o horario de checkin do hotel (hh:mm): ");
-        scanf("%d:%d", &hotel.checkin[0], &hotel.checkin[1]);
-
-        printf("Informe o horario de checkout do hotel (hh:mm): ");
-        scanf("%d:%d", &hotel.checkout[0], &hotel.checkout[1]);
 
         if(checkInfo())
         {
-            printf("salvo");
+            FILE *f;
+            if((f = fopen("..\\Saves\\hotel.txt", "w")) == NULL)//abre o arquivo
+            {
+                printf("Erro ao salvar os dados, tente novamente!\n");
+                continue;
+            }
+            
+            if(fwrite(&hotel, sizeof(hotel), 1, f) != 1)//escreve no arquivo
+            {
+                printf("Erro ao salvar no arquivo!\n");
+                continue;
+            }
             //salva no arquivo
-            return 1;
+            fclose(f);
+            break;
         }
     }
 }
 
-void addProd(void)//adicionar produtos
+void addProd(void)//adicionar produtos (    TESTAR      )
 {
     tipoProdutos adProduto;
     while(1)
@@ -103,4 +111,77 @@ void addProd(void)//adicionar produtos
             //salvar no arquivo
         }
     }
+}
+
+void gerente(void)//apenas criar função que altera o local de save
+{
+    tipoGerente gerente;
+
+    printf("Informe o nome do gerente do hotel: ");
+    fgets(gerente.nome, 99, stdin);
+
+    do{
+        printf("Informe o telefone do gerente do hotel (ddnnnnnnnnn/11 digitos): ");
+        scanf("%11[0-9]", &gerente.telefone);
+        setbuf(stdin, NULL);            
+    }while(strlen(gerente.telefone) != 11);
+
+    printf("Nome: %sTelefone: %s\n\n", gerente.nome, gerente.telefone);
+
+    if(checkInfo())
+    {
+        FILE *fGerente;
+        fGerente = fopen("..\\Saves\\gerente.txt", "w");
+        fprintf(fGerente, "%s\r\n%s", gerente.nome, gerente.telefone);
+        fclose(fGerente);
+
+        printf("Informacoes salvas em: %s", "Saves\\gerente.txt");//caminho
+    }
+}
+
+int checkInfo(void)//CONCLUIDA
+{
+    char op;
+    do{
+            printf("As informacoes estao corretas? (s - sim / n - nao): ");
+            scanf("%c%*c", &op);
+
+            if('S' == toupper(op))
+            {
+                return 1;
+            }else
+            if('N' == toupper(op))
+            {
+                printf("Faca as alteracoes!\n\n");
+                return 0;
+            }
+            printf("Opcao invalida, tente novamente!\n");
+        }while(toupper(op) != 'N' && toupper(op) != 'S');
+}
+
+void cleanBuff(void)//limpa o buffer (CONCLUIDO)
+{
+    int ch = 0;
+    while((ch = getchar()) != '\n' && ch != EOF){}
+}
+
+int checkHorario(int horario, char tipo)//concluida
+{   
+    int r;
+    switch (tipo)
+    {
+        case 'h'://hora
+            if(horario > 24 || horario < 0)
+                r = 1;
+            break;
+        case 'm'://minuto
+            if(horario > 60 || horario < 0)
+                r = 1;
+            break;
+    }
+    if(r == 1){
+        printf("\nHorario invalido, tente novamente!\n");
+        return 1;
+    }  
+    return 0;
 }
--- Arquivos/Gerenciamento de dados/main.c
+++ Arquivos/Gerenciamento de dados/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dados.h"
 
-int main()
+ int main(void)//editar hotel
 {
+    tipoHotel hotel;
+    memset(&hotel, 'a', sizeof(hotel));//setar todo esse pedaço de memoria com o caracter de delete
 
-    return 0;
+  return 0;
 }
